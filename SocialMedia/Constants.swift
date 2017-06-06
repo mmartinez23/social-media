971b6f49f77c559033fafcaed9f0b7deca4ce5a7
Mike <mikewazaoski@Mikes-MacBook-Pro.local>
2017-06-06 16:34:39 -0400

    Custom UI classes

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Mike on 6/4/17.
+//  Copyright © 2017 Mike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
